57d38a8fd52c0d0763ddff35019920be71425384
Alexey Gaziev <alex.gaziev@gmail.com>
2015-03-29 15:37:35 +0300

    Add tests for plugin

--- test/test.js
+++ test/test.js
@@ -3,16 +3,46 @@ var expect  = require('chai').expect;
 
 var plugin = require('../');
 
-var test = function (input, output, opts) {
-    expect(postcss(plugin(opts)).process(input).css).to.eql(output);
+var warninger = function (css, result) {
+    result.warn("Here is some warning");
+};
+var warninger2 = function (css, result) {
+    result.warn("Here is another warning");
+};
+
+var before = function (content) {
+                 return 'a{ }\n' +
+                        'body:before{\n' +
+                        '    background: red;\n' +
+                        '    display: block;\n' +
+                        '    padding: 20px 30px;\n' +
+                        '    color: white;\n' +
+                        '    font-size: 16px;\n' +
+                        '    white-space: pre;\n' +
+                        '    content: "Here is some warning' + content + '"\n' +
+                        '}';
+             };
+
+var test = function (input, output, plugins) {
+    expect(postcss(plugins).process(input).css).to.eql(output);
 };
 
 describe('postcss-messages', function () {
 
-    /* Write tests here
+    it('displays warning before body', function () {
+        test('a{ }', before(''), [warninger, plugin()]);
+    });
 
-    it('does something', function () {
-        test('a{ }', 'a{ }');
-    });*/
+    it('not displays warning before body if disabled', function () {
+        test('a{ }', 'a{ }', [warninger, plugin({ disabled: true })]);
+    });
+
+    it('displays two warnings from two plugins on new lines', function () {
+        test('a{ }', before('\\00000aHere is another warning'), [warninger, warninger2, plugin({})]);
+    });
+
+    it('displays only warnings from plugins before', function () {
+        test('a{ }', before(''), [warninger, plugin({}), warninger2]);
+    });
 
 });
