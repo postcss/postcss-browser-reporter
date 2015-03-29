00a152e78e3d47dce62e72a9972b0f940ac12ce3
Alexey Gaziev <alex.gaziev@gmail.com>
2015-03-29 15:37:06 +0300

    Plugin implementation

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-module.exports = function (opts) {
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-messages', function (opts) {
     opts = opts || {};
 
-    // Work with options here
+    if ( opts.disabled === true ) {
+        return function () { };
+    }
 
-    return function (css) {
+    return function (css, result) {
+        css.append({ selector: 'body:before' });
+        css.last.append({ prop: 'background', value: 'red' });
+        css.last.append({ prop: 'display', value: 'block' });
+        css.last.append({ prop: 'padding', value: '20px 30px' });
+        css.last.append({ prop: 'color', value: 'white' });
+        css.last.append({ prop: 'font-size', value: '16px' });
+        css.last.append({ prop: 'white-space', value: 'pre' });
 
-        // Transform CSS AST here
+        var content = result.warnings().map(function (message) {
+            return message.toString().replace(/"/g, '\\"');
+        }).join('\\00000a');
 
+        css.last.append({ prop: 'content', value: '"' + content + '"' });
     };
-};
-module.exports.postcss = function (css) {
-    return module.exports()(css);
-};
+});
