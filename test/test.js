e4d2c896c3335b169feaa1c9b5ee067b346f56ce
Alexey Gaziev <alex.gaziev@gmail.com>
2015-04-03 17:43:10 +0800

    Rewrite default styles

--- test/test.js
+++ test/test.js
@@ -10,16 +10,30 @@ var warninger2 = function (css, result) {
     result.warn("Here is another warning");
 };
 
+var beforeCustom = 'a{ }\n' +
+           'html:before{\n' +
+           '    text-align: center;\n' +
+           '    content: "Here is some warning"\n' +
+           '}';
+
 var before = function (opts) {
     return 'a{ }\n' +
            ( opts && opts.selector ? opts.selector : 'html:before' ) + '{\n' +
            '    display: block;\n' +
-           '    padding: 20px 30px;\n' +
-           '    background: red;\n' +
-           '    font-size: 16px;\n' +
-           '    color: ' + ( opts && opts.changedColor ? opts.changedColor : 'white' ) + ';\n' +
+           '    position: fixed;\n' +
+           '    top: 0;\n' +
+           '    left: 0;\n' +
+           '    right: 0;\n' +
+           '    font-size: .9em;\n' +
+           '    padding: 1.5em 1em 1.5em 4.5em;\n' +
+           '    color: white;\n' +
+           '    background: #DF4F5E url("data:image/svg+xml;charset=utf-8,%3Csvg%20version%3D%221.1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20x%3D%220px%22%20y%3D%220px%22%20width%3D%2248px%22%20height%3D%2248px%22%20viewBox%3D%220%200%20512%20512%22%20enable-background%3D%22new%200%200%20512%20512%22%20xml%3Aspace%3D%22preserve%22%3E%3Cpath%20fill%3D%22%23A82734%22%20id%3D%22warning-4-icon%22%20d%3D%22M228.55%2C134.812h54.9v166.5h-54.9V134.812z%20M256%2C385.188c-16.362%2C0-29.626-13.264-29.626-29.625c0-16.362%2C13.264-29.627%2C29.626-29.627c16.361%2C0%2C29.625%2C13.265%2C29.625%2C29.627C285.625%2C371.924%2C272.361%2C385.188%2C256%2C385.188z%20M256%2C90c91.742%2C0%2C166%2C74.245%2C166%2C166c0%2C91.741-74.245%2C166-166%2C166c-91.742%2C0-166-74.245-166-166C90%2C164.259%2C164.245%2C90%2C256%2C90z%20M256%2C50C142.229%2C50%2C50%2C142.229%2C50%2C256s92.229%2C206%2C206%2C206s206-92.229%2C206-206S369.771%2C50%2C256%2C50z%22%2F%3E%3C%2Fsvg%3E") .5em no-repeat, linear-gradient(#DF4F5E, #CE3741);\n' +
+           '    border: 1px solid #C64F4B;\n' +
+           '    border-radius: 3px;\n' +
+           '    box-shadow: inset 0 1px 0 #EB8A93, 0 0 .3em rgba(0,0,0, .5);\n' +
            '    white-space: pre;\n' +
-           ( opts && opts.addedStyle ? ('    ' + opts.addedStyle + ';\n') : '') +
+           '    font-family: Menlo, Monaco, monospace;\n' +
+           '    text-shadow: 0 1px #A82734;\n' +
            '    content: "Here is some warning' + ( opts && opts.content ? opts.content : '' ) + '"\n' +
            '}';
 };
@@ -50,12 +64,8 @@ describe('postcss-messages', function () {
         test('a{ }', before({ selector: 'body:before' }), [warninger, plugin({ selector: 'body:before' })]);
     });
 
-    it('displays warning with custom font color', function () {
-        test('a{ }', before({ changedColor: 'gray' }), [warninger, plugin({ styles: { color: 'gray' } })]);
-    });
-
-    it('displays warning with custom style property ', function () {
-        test('a{ }', before({ addedStyle: 'text-align: center' }), [warninger, plugin({ styles: { 'text-align': 'center' } })]);
+    it('displays warning with custom styles ', function () {
+        test('a{ }', beforeCustom, [warninger, plugin({ styles: { 'text-align': 'center' } })]);
     });
 
 });
