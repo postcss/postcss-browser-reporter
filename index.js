9fb218420d4af8f35c771a63bb99ca856799b83b
Alexey Gaziev <alex.gaziev@gmail.com>
2015-04-03 13:28:56 +0800

    Add styles from options logic

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-messages', function (opts) {
     if ( opts && opts.disabled === true ) {
         return function () { };
-    }
+    };
+
+    var styleKeys = ['display', 'padding', 'background', 'font-size', 'color', 'white-space'];
+
+    var defaultStyles = {
+      'display': 'block',
+      'padding': '20px 30px',
+      'background': 'red',
+      'font-size': '16px',
+      'color': 'white',
+      'white-space': 'pre'
+    };
+    if ( opts && opts.styles ) {
+      for ( var attrname in opts.styles ) {
+        if ( opts.styles.hasOwnProperty(attrname) ) {
+          defaultStyles[attrname] = opts.styles[attrname];
+        };
+      };
+    };
 
     return function (css, result) {
-        css.append({ selector: 'body:before' });
-        css.last.append({ prop: 'display', value: 'block' });
-        css.last.append({ prop: 'padding', value: '20px 30px' });
-        css.last.append({ prop: 'background', value: 'red' });
-        css.last.append({ prop: 'font-size', value: '16px' });
-        css.last.append({ prop: 'color', value: 'white' });
-        css.last.append({ prop: 'white-space', value: 'pre' });
+        css.append({ selector: (opts && opts.selector) ? opts.selector : 'body:before' });
+        for ( var style in defaultStyles ) {
+          if ( defaultStyles.hasOwnProperty(style) ) {
+            css.last.append({ prop: style, value: defaultStyles[style] });
+          }
+        };
 
         var content = result.warnings().map(function (message) {
             return message.toString().replace(/"/g, '\\"');
